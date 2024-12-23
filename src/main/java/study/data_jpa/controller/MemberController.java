7bdba086800e2d7cec4155910b0e7a28b8001e8c
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-23 20:13:16 +0900

    feat: paging, sorting

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,9 +2,13 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -28,8 +32,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for(int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
