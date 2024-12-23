4a93cb863effeb54bcc0f9970314ba707512f4c7
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-24 06:39:54 +0900

    feat: projections

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -65,4 +65,8 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
+
+    <T> List<T> findProjectionsByUsername(@Param("username") String username, Class<T> type);
+
+
 }
